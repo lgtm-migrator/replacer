3a2221fc8463f941b347f47a662e4fe9644b59a5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-01-10 11:19:34 +0200

    moving code into main branch

---
+++ react/replacer/src/index.js
@@ -0,0 +1,18 @@
+import {
+    generateReactContent,
+    generateReactFullTemplate
+} from './methods'
+
+import {
+    parseMDReact, 
+    parseMDReactFullThing
+} from './parse'
+// import 
+
+export default {
+    generateReactContent,
+    generateReactFullTemplate,
+
+    parseMDReact, 
+    parseMDReactFullThing
+}
